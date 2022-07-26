1dcf68eb0d5a455143a201c9af16c71bdc1259a9
huangyinhaow <huangyinhaow@outlook.com>
2022-07-26 20:57:06 +0800

    update

--- pages/sendId.js
+++
@@ -1,7 +0,0 @@
-function sendId() {
-  let id = document.getElementById("inn").value;
-  let url = String("./api/getCid?id=" + id);
-  let cid = fetch(url);
-  cid = String(cid.cid);
-  return cid;
-}
